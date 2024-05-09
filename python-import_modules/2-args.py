fa2d6e681e8210a8063e0170383c842924214223
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-09 11:52:28 +0200

    minor changes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,11 +3,7 @@ import sys
 
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    print("{:d} {}{}".format(
-        argc,
-        "argument" if argc == 1 else "arguments",
-        "." if argc < 1 else ":"
-    ))
+    print("{} {}{}".format(argc, "argument" if argc == 1 else "arguments", "." if argc < 1 else ":"))
     if argc >= 1:
         for i in range(1, len(sys.argv)):
             arg = sys.argv[i]
