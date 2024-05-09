33e78b619af9854744f5a3ae67d22ce7120a3205
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-09 12:00:10 +0200

    change start at range 1

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ import sys
 if __name__ == "__main__":
 
     result = 0
-    for arg in range(len(sys.argv)):
+    for arg in range(1, len(sys.argv)):
         result += int(sys.argv[arg])
     print("{}".format(result))
