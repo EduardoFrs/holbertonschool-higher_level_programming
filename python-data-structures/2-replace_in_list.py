ad24e15f6ac0ea698efec88c4fa2ae3dd699631a
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-13 10:43:17 +0200

    function that replace and element of a list as a given index

---
+++ python-data-structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/use/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return (my_list)
+    if idx > my_list[idx]:
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return my_list
