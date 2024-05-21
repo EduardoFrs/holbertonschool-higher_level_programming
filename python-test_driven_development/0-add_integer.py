5b2e5232df15c0adeaeca496ac0b2e8a7f0dc49c
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-21 10:46:00 +0200

    add documentation

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""Function for add two ints"""
+
+
 def add_integer(a, b=98):
+    """
+    add 2 int
+    return: result of add
+    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
