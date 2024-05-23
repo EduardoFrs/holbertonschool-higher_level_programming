cc1205b25e6023a66f0df055388d7b3d63552a12
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-23 12:53:18 +0200

    fix error

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -105,9 +105,14 @@ class Rectangle:
         Rectangle.number_of_instances -= 1
 
     def bigger_or_equal(rect_1, rect_2):
+        """return biggest rectangle"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1 == rect_2:
+        area_1 = rect_1.area()
+        area_2 = rect_2.area()
+        if area_1 >= area_2:
             return rect_1
+        else:
+            return rect_2
