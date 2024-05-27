2d0a20d32ca0a977e2886cba7a21737a3cb91715
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-27 12:29:52 +0200

    filter attribute

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -10,4 +10,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs=None):
-        return self.__dict__
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {key: value for key, value in self.__dict__.items()
+                    if key in attrs}
