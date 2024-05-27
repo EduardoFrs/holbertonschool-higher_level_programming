ea00cf23e34aa5003cd8d97ca49478049b6acfdd
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-27 11:58:12 +0200

    remove the import

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -2,15 +2,8 @@
 """8. Class to JSON"""
 
 
-import json
-
-
 def class_to_json(obj):
     """function that return the dictionnary description
     with simple data structure for
     JSON serialization of an object"""
-    json_dict = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            json_dict[key] = value
-    return json_dict
+    return obj.__dict__
