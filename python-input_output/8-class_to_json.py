09f62cd49cab10662e438975d13908338bcc0a25
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-27 11:55:47 +0200

    filtering of attribute

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -9,4 +9,8 @@ def class_to_json(obj):
     """function that return the dictionnary description
     with simple data structure for
     JSON serialization of an object"""
-    return obj.__dict__
+    json_dict = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            json_dict[key] = value
+    return json_dict
