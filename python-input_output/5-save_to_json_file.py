89f8fd6bf74a0e7139e0462537c9ad0625a9384c
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-27 10:48:09 +0200

    fix errors

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -9,4 +9,4 @@ def save_to_json_file(my_obj, filename):
     """function that write an object to a txt file,
     using JSON representation"""
     with open(filename, 'w', encoding="utf-8") as f:
-        return json.dumps(my_obj)
+        return json.dump(my_obj, f)
