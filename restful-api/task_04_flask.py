9a33ab2c75a36a08661300e36aa119eb02061997
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-31 11:19:07 +0200

    add route for status and users

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 from flask import Flask
 from flask import jsonify
+from flask import request
 
 
 app = Flask(__name__)
@@ -15,6 +16,16 @@ def home():
 @app.route("/data")
 def data():
     return jsonify(list(users.keys()))
+
+
+@app.route("/status")
+def status():
+    return "OK"
+
+
+@app.route("/users/<username>")
+def show_username(username):
+    return jsonify(users[username])
 
 
 if __name__ == "__main__":
