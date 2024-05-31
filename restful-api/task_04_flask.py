144e71553a233a8b75d310bd0a4b8d899bd40a85
EduardoFrs <fernandeseduardo00@gmail.com>
2024-05-31 10:58:15 +0200

    add json data

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 from flask import Flask
+from flask import jsonify
+
 
 app = Flask(__name__)
+users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 
 
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def data():
+    return jsonify(list(users.keys()))
 
 
 if __name__ == "__main__":
