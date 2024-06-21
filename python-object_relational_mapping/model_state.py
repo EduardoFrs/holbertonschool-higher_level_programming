b88609d9760089773568c8f014857e8311ffe447
EduardoFrs <fernandeseduardo00@gmail.com>
2024-06-21 17:17:53 +0200

    fix

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -19,5 +19,5 @@ class State(Base):
         primary_key=True)
     name = Column(String(128), nullable=False)
 
-    engine = create_engine('mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa')
-    Base.metadata.create_all(engine)
+engine = create_engine('mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa')
+Base.metadata.create_all(engine)
