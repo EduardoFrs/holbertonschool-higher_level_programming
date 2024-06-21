e622aabf29ce0de8cc4d0cfc6e2f500adcaa43bf
EduardoFrs <fernandeseduardo00@gmail.com>
2024-06-21 17:07:57 +0200

    add model state

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -18,3 +18,6 @@ class State(Base):
         nullable=False,
         primary_key=True)
     name = Column(String(128), nullable=False)
+
+    engine = create_engine('mysql+mysqldb://root:@localhost:3306/hbtn_0e_6_usa')
+    Base.metadata.create_all(engine)
