6ccca4ca920cb568a1736decc516c83157060114
EduardoFrs <fernandeseduardo00@gmail.com>
2024-06-21 17:23:49 +0200

    je sais pas

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -10,14 +10,10 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = 'states'
-    id = Column(
-        Integer,
-        autoincrement=True,
-        unique=True,
-        nullable=False,
-        primary_key=True)
+    __tablename__ = "states"
+    id = Column(Integer, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
+
 
 engine = create_engine('mysql+mysqldb://root:root@localhost:3306/hbtn_0e_6_usa')
 Base.metadata.create_all(engine)
